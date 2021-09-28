a651521d061ca01fe0262bb74508965dcfe2a68b
Gavin Scanlon <gscanlon@uncc.edu>
2021-09-27 21:31:36 -0400

    PythonBasics 2 – added logic to is_palindrome function

--- PythonBasics2/pythonBasics2.py
+++ PythonBasics2/pythonBasics2.py
@@ -43,6 +43,6 @@ def longest_consecutive_repeating_char(s):
 # forwards. Treat capital letters the same as lowercase ones
 # and ignore spaces (i.e. case insensitive).
 def is_palindrome(s):
-  # YOUR CODE HERE
-
-  return
+  str = s.lower()
+  str = str.replace(" ", "")
+  return str == str[::-1]
