997185a92be199fab16bdcc96182c3932f093770
Gavin Scanlon <gscanlon@uncc.edu>
2021-10-05 09:08:40 -0400

    PythonBasics2 - Update methods

--- PythonBasics2/pythonBasics2.py
+++ PythonBasics2/pythonBasics2.py
@@ -11,31 +11,53 @@
 # to n (including n).
 
 def count_threes(n):
-  x = n/3
+  n = list(str(n))
+  max = 0
+  ind = 0
+  umap = {}
+  umap[3] = 0
+  umap[6] = 0
+  umap[9] = 0
+  for i in n:
+    x = int(i)
+    if x % 3 == 0 and x != 0:
+      umap[x] += 1
+  for i,j in umap.items():
+    if j > max:
+      max = j
+      ind = i
+  return ind
 
-  return int(x)
+
 
 
 # Part B. longest_consecutive_repeating_char
 # Define a function longest_consecutive_repeating_char(s) that takes
 # a string s and returns the character that has the longest consecutive repeat.
 def longest_consecutive_repeating_char(s):
-  count = 0
-  max = 0
-  c = s[0]
+  s = list(s)
+  count = 1
+  max = 1
+  l = []
+  umap = {}
   for i in range(0, len(s) - 1):
     if s[i] == s[i+1]:
       count += 1
     else:
-      if count > max:
-        max = count
+      if((s[i] in umap) and umap[s[i]] > count):
+        continue
+      else:
+        umap[s[i]] = count
+        count = 1
         c = s[i]
-      count = 1
-  if count > max:
-    max = count
-    c = s[i]
-  return c
-
+  umap[s[len(s) - 1]] = count
+  for i,j in umap.items():
+    if j > max:
+      max = j
+  for i,j in umap.items():
+    if j == max:
+      l.append(i)
+  return l
 # Part C. is_palindrome
 # Define a function is_palindrome(s) that takes a string s
 # and returns whether or not that string is a palindrome.
